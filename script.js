608e1dfab651c64426c1f0ff371ec01784fb680f
ronivelton martins romão <roniveltonmartins2022@gmail.com>
2025-01-23 21:23:19 -0400

    add evento de click

--- script.js
+++ script.js
@@ -20,18 +20,27 @@ let imgCheck = document.createElement("img")
   let NewSpan = document.createElement("span")
   NewSpan.textContent = input.value
   let imgTrash = document.createElement("img")
-  imgTrash.src ="assets/TrashRegular.png";
-
-
-  
+  imgTrash.src ="assets/TrashRegular.png";  
 lista.append(lis)
 lis.append(imgCheck, NewSpan, imgTrash)
 
 
+let isChecked = false;
 
-
-
-
+imgCheck.addEventListener("click", function () {
+  if (isChecked) {
+    imgCheck.src = "assets/CircleDuotone.png"; // Substitua pelo caminho da imagem original
+    NewSpan.style.textDecoration = "none"
+    NewSpan.style.color = "var(--gray-600)"
+  } else {
+    imgCheck.src = "assets/CheckCircleFill.svg";
+     NewSpan.style.textDecoration = "line-through";
+     NewSpan.style.color = "var(--gray-500)"
+    
+  }
+  isChecked = !isChecked; // Alterna o estado
+});
+input.value = ""
 
 
 })
